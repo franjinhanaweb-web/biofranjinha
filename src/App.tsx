a8c5f28c052596c90d447ad27693061562c2fc4d
franjinhanaweb-web <franjinhanaweb@gmail.com>
2025-09-08 22:59:01 -0300

    Limpeza final: remover todas as referências ao App Check
    - Deletar arquivos de documentação restantes com referências ao App Check
    - Remover import e rota do AppCheckDemo no App.tsx
    - Limpar completamente todas as referências ao App Check do projeto
    - Manter apenas as dependências no package-lock.json (serão removidas automaticamente)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import AuthPage from './pages/AuthPage/AuthPage';
 import LandingPage from './pages/LandingPage/LandingPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
-import AppCheckDemo from './pages/AppCheckDemo';
 import { UserData } from './services/authService';
 import './App.css';
 
@@ -72,10 +71,6 @@ function AppContent() {
             <Navigate to="/" replace />
           )
         } 
-      />
-      <Route 
-        path="/app-check-demo" 
-        element={<AppCheckDemo />} 
       />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
