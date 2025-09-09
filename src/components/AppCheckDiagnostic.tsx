9f13680f29baefc63af74de7225751f862a27ea5
franjinhanaweb-web <franjinhanaweb@gmail.com>
2025-09-09 13:46:45 -0300

    Fix ESLint warning - remove unused useEffect import

--- src/components/AppCheckDiagnostic.tsx
+++ src/components/AppCheckDiagnostic.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useAppCheck } from '../hooks/useAppCheck';
 
 const AppCheckDiagnostic: React.FC = () => {
