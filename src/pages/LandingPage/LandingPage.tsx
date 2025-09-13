0aad8972f12132622ff651987a87f55097174aee
franjinhanaweb-web <franjinhanaweb@gmail.com>
2025-09-12 22:19:52 -0300

    feat: adicionar link 'Agenda Nova' na navbar para www.afranjinha.com.br/agenda

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -33,7 +33,8 @@ const LandingPage: React.FC<LandingPageProps> = ({ onNavigateToLogin, onNavigate
     { label: 'Início', href: '#home' },
     { label: 'Experiências', href: '#experiencias' },
     { label: 'Sobre', href: '#sobre' },
-    { label: 'Agenda', href: 'https://www.afranjinha.com.br/agenda', isHighlighted: true }, // Updated link
+    { label: 'Agenda', href: 'https://calendarv3.pages.dev/booking', isHighlighted: true },
+    { label: 'Agenda Nova', href: 'https://www.afranjinha.com.br/agenda', isHighlighted: true },
     { label: 'Localização', href: '#localizacao' },
     { label: 'Preferências', href: '#preferencias' },
     { label: 'Pagamento', href: '#pagamento' },
